0db19bd4838f70bd0face8d75acaecd151caaaeb
Mustafa Cantürk Karabulut <karabulut_3828@hotmail.com>
2025-03-02 15:56:18 +0300

    Initial commit

---
+++ app/src/main/java/com/rickandmorty/rickandmortymultimodule/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.rickandmorty.rickandmortymultimodule
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.rickandmorty.network.TestFunction
+import com.rickandmorty.rickandmortymultimodule.ui.theme.RickAndMortyMultiModuleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RickAndMortyMultiModuleTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    //Greeting(
+                    //    name = "Android",
+                    //    modifier = Modifier.padding(innerPadding)
+                    //)
+                    TestFunction()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    RickAndMortyMultiModuleTheme {
+        Greeting("Android")
+    }
+}
---
+++ network/src/main/java/com/rickandmorty/network/TestFile.kt
@@ -0,0 +1,10 @@
+package com.rickandmorty.network
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TestFunction() {
+    Text(text = "Hello Other Network Module")
+
+}
